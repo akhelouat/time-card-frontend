09928d6e4b50635947be574b0af10619afc69642
rahil-f <felixrahil@yahoo.fr>
2020-02-18 16:36:32 +0100

    all file new pw

---
+++ src/components/Present.vue
@@ -0,0 +1,24 @@
+<!--
+ * @ Author: Rahil Felix
+ * @ Create Time: 2020-02-18 12:02:04
+ * @ Modified by: Rahil Felix
+ * @ Modified time: 2020-02-18 16:24:27
+ * @ Description:
+ -->
+
+<template>
+    <div>
+        <meta http-equiv="refresh" content="2;URL=http://localhost:8080/">
+
+        <img  src="https://thumbs.gfycat.com/LonelyMinorBlacklemur-size_restricted.gif" alt="" /> </div>
+</template>
+
+<script>
+    export default {
+       
+    };
+</script>
+
+<style>
+
+</style>>
