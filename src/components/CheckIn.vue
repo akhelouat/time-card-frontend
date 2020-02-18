e8ae5aeb3df26af6f4d12ee83ff8083d02b48c91
KHELOUATAbdelMalek <ab.khelouat@gmail.com>
2020-02-18 15:53:21 +0100

    #4: display the current time

--- src/components/CheckIn.vue
+++ src/components/CheckIn.vue
@@ -5,18 +5,39 @@
  * @ Modified time: 2020-02-18 13:26:53
  * @ Description:
  -->
+ 
 
 <template>
     <div>
         <form action='abdelfaitdelapdo' method="post">
+            <h3>We are : <span> {{ createdAtDisplay }}</span></h3>
             <button type="submit">benjam prezent</button>
+            {{ checkTime }}
         </form>
     </div>
 </template>
 
 <script>
+    const moment = require('moment')
     export default {
-        
+        computed: {
+            createdAtDisplay() {
+                return moment().format('YYYY-MM-DD h:mm A');
+            }
+        },
+        methods: {
+            checkTime() {
+                var format = 'hh:mm:ss'
+                var time = moment('09:31:00', format),
+                    beforeTime = moment('09:30:00', format),
+                    afterTime = moment('17:00:00', format);
+                if (time.isBetween(beforeTime, afterTime)) {
+                    console.log('is between')
+                } else {
+                    console.log('is not between')
+                }
+            }
+        }
     };
 </script>
 
