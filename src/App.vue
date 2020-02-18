a233ee282d294bb5f82cf4823eb91eb0b89621d6
rahil-f <felixrahil@yahoo.fr>
2020-02-18 14:34:31 +0100

    pwd

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,37 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
- <CheckIn />
- <Footer />
-
+    <CheckIn />
+    <Footer />
+    <NewPwd/>
   </div>
 </template>
 
 <script>
-import CheckIn from "./components/CheckIn.vue"
-import Footer from "./components/footer.vue"
-export default {
-  name: 'App',
-  components: {
-    CheckIn,
-    Footer
+  import CheckIn from "./components/CheckIn.vue"
+  import Footer from "./components/footer.vue"
+  import NewPwd from "./components/NewPwd.vue"
+  export default {
+    name: 'App',
+    components: {
+      CheckIn,
+      Footer,
+      NewPwd
+    }
   }
-}
 </script>
 
 <style>
-body{
-  margin: 0px;
-}
-#app {
-  height: 100%;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0px;
+  }
+  #app {
+    height: 100%;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
