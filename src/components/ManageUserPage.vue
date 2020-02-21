e0ad039e37e04d4672224a9b3cca78a3804bfb6a
stephane <stephane.vieira75@outlook.fr>
2020-02-21 01:33:58 +0100

    #19 update: correcting errors - better component template - adding style in ManageUserPage.vue

--- src/components/ManageUserPage.vue
+++ src/components/ManageUserPage.vue
@@ -1,40 +1,54 @@
 <template>
     <div>
         <aside class="container">
-            <!-- Select promo -->
-            <div class="selector-container">
-                <select class="promo-selector" name="promo" v-model="selectedPromo">
-                    <option value="default">Please choose a promo</option>
-                    <option value="web">Dev Web</option>
-                    <option value="ops">Dev Ops</option>
-                </select>
-            </div>
-
             <!-- Students promo -->
             <div class="links-container">
-                <h1>Students:</h1>
-
-                <!-- Web promo links -->
-                <ol v-if="selectedPromo === 'web'" class="links-list">
-                    <li v-for="user in users" :key="user">
-                        <a v-if="user.promo === 'web'" href="#">{{user.firstName + ' ' + user.lastName }}</a>
-                    </li>
-                </ol>
-
-                <!-- Ops promo links -->
-                <ol v-if="selectedPromo === 'ops'" class="links-list">
-                    <li v-for="user in users" :key="user">
-                        <a v-if="user.promo === 'ops'" href="#">{{user.firstName + ' ' + user.lastName }}</a>
-                    </li>
-                </ol>
-
-                <div v-else></div>
+                <!-- Aside title -->
+                <span class="title-container">
+                        <h1>{{ selectedPromo }} Students:</h1>
+                    </span>
+                <!-- Select promo -->
+                <select class="promo-selector" name="promo" v-model="selectedPromo">
+                        <option value="">Please choose a promo</option>
+                        <option value="Web">Dev Web</option>
+                        <option value="Ops">Dev Ops</option>
+                    </select>
+                <!-- Student promo container -->
+                <div class="promo-container" v-for="usr in users" :key="usr.firstName">
+                    <!-- Web promo links -->
+                    <span class="link-container">
+                            <a href="#" v-if="usr.promo && usr.promo === promos[1].name && selectedPromo === promos[1].name" @click="getSelectedStudent(usr)">{{usr.firstName + ' ' + usr.lastName }}</a>
+                        </span>
+                    <!-- Ops promo links -->
+                    <span class="link-container">
+                            <a href="#" v-if="usr.promo && usr.promo === promos[0].name && selectedPromo === promos[0].name" @click="getSelectedStudent(usr)">{{usr.firstName + ' ' + usr.lastName }}</a>
+                        </span>
+                </div>
             </div>
         </aside>
-
-        <!-- Student -->
+        <!-- Student Updater -->
         <div>
             <h1>{{ title }}</h1>
+            <!-- Form update -->
+            <div class="form-control" v-if="selectedStudent.student !==''">
+                <!-- User names -->
+                <div class="test">
+                    <input type="text" name="fist-name" :value="selectedStudent.student.firstName" />
+                    <input type="text" name="last-name" :value="selectedStudent.student.lastName" />
+                </div>
+                <!--  User address -->
+                <div class="test">
+                    <input type="text" name="address" :value="selectedStudent.student.address" />
+                    <input type="text" name="address-cp" placeholder="CP" :value="selectedStudent.student.addressCP" />
+                </div>
+                <input type="text" name="mail" :value="selectedStudent.student.mail" />
+                <input type="text" name="mobile-number" :value="selectedStudent.student.mobileNumber" />
+                <input type="text" name="pole-emploi-number" :value="selectedStudent.student.poleEmploiNumber" />
+                <button href="#">Update User</button>
+            </div>
+            <div v-else>
+                <p>Select promo in the panel then choose a student</p>
+            </div>
         </div>
     </div>
 </template>
@@ -52,7 +66,7 @@
                         addressCP: "75013",
                         mobileNumber: "07 08 89 10 11",
                         poleEmploiNumber: "K72AZ6",
-                        promo: "web"
+                        promo: "Web"
                     },
                     {
                         firstName: "Felix",
@@ -62,7 +76,7 @@
                         addressCP: "75020",
                         mobileNumber: "07 12 13 10 11",
                         poleEmploiNumber: "L97AZ6",
-                        promo: "web"
+                        promo: "Web"
                     },
                     {
                         firstName: "Ali",
@@ -72,7 +86,7 @@
                         addressCP: "94155",
                         mobileNumber: "07 89 98 78 87",
                         poleEmploiNumber: "P182A8",
-                        promo: "web"
+                        promo: "Web"
                     },
                     {
                         firstName: "Ghiles",
@@ -82,7 +96,7 @@
                         addressCP: "75019",
                         mobileNumber: "07 12 31 41 51",
                         poleEmploiNumber: "J114B6",
-                        promo: "ops"
+                        promo: "Ops"
                     },
                     {
                         firstName: "John",
@@ -92,7 +106,7 @@
                         addressCP: "93270",
                         mobileNumber: "07 56 65 45 54",
                         poleEmploiNumber: "J114B6",
-                        promo: "ops"
+                        promo: "Ops"
                     },
                     {
                         firstName: "Fabien",
@@ -102,42 +116,72 @@
                         addressCP: "75019",
                         mobileNumber: "07 12 31 41 51",
                         poleEmploiNumber: "J114B6",
-                        promo: "ops"
+                        promo: "Ops"
                     }
                 ],
-                selectedPromo: "default"
+                promos: [{
+                        name: 'Ops'
+                    },
+                    {
+                        name: 'Web'
+                    }
+                ],
+                selectedPromo: "",
+                selectedStudent: {
+                    student: ""
+                }
             };
+        },
+        methods: {
+            getSelectedStudent(student) {
+                return this.selectedStudent = {
+                    student
+                };
+            }
         }
     }
 </script>
 
 <style scoped>
     aside {
-        background-color: #121212;
+        background-color: #313131;
         position: fixed;
         left: 0;
-        height: 100%;
-        width: 15%;
+        height: 80%;
+        width: 18%;
     }
     .links-container {
         display: flex;
         flex-direction: column;
-        align-items: center;
     }
-    .links-list {
-        list-style-type: none;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
+    .title-container {
+        width: 100%;
+        background-color: #a5223e;
+        color: #fafafa;
     }
-
-    .promo-selector{
+    .promo-selector {
         width: 100%;
     }
-
     a {
-        font-size: 1.5em;
+        font-size: 1.2em;
         color: #fafafa;
         text-decoration: none;
+    }
+    .form-control {
+        display: flex;
+        flex-direction: column;
+        width: 50%;
+        margin-left: 30%;
+    }
+    input,
+    button {
+        width: 80%;
+        margin: 15px 0 0 0;
+    }
+    .test {
+        display: flex;
+    }
+    .test>input {
+        width: 39%;
     }
 </style>
