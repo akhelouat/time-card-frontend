42a822d1ac9e4aa4c607bd4ec010623dd096ef38
KHELOUAT Abdelmalek <ab.khelouat@gmail.com>
2020-02-25 22:58:22 +0100

    create addPromo() function in data.js to send a create request to api

--- src/services/api/data.js
+++ src/services/api/data.js
@@ -1,5 +1,12 @@
 const axios = require('axios');
 
+export function addPromo() {
+    return axios.request({
+        method: 'post',
+        url: '/api/addPromo'
+    })
+    .then(response => response.data);
+}
 
 export function data() {
     return axios
