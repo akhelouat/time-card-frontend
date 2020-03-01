811deb82b12c378ea0621bc3c5010e62d30b2701
stephane <stephane.vieira75@outlook.fr>
2020-03-01 21:44:10 +0100

    move test into crashtest and add new form in UserCreator

--- src/components/ManagerComponents/UserCreator.vue
+++ src/components/ManagerComponents/UserCreator.vue
@@ -1,92 +1,168 @@
 <template>
-    <div class="form-control">
-        <h1>{{ title }}:</h1>
-        <!-- User names -->
-        <div class="control-container">
-            <input type="text" name="fist-name" placeholder="First Name" v-model="user.firstName" />
-            <input type="text" name="last-name" placeholder="Last name" v-model="user.lastName" />
-        </div>
-        <!--  User address -->
-        <div class="control-container">
-            <input type="text" name="address" placeholder="Address" v-model="user.address" />
-            <input type="text" name="address-cp" placeholder="CP address" v-model="user.addressCP" />
-        </div>
-        <input type="text" name="mail" placeholder="mail" v-model="user.mail" />
-        <input type="text" name="mobile-number" placeholder="mobile" v-model="user.mobileNumber" />
-        <input type="text" name="pole-emploi-number" placeholder="pole emploi Id" v-model="user.poleEmploiNumber" />
-        <select class="promo-selector" name="promo" v-model="user.namePromo">
-                <option value="">Please choose a promo</option>
-                <option v-for="promo in promos" :key="promo.name" :value="promo.name" >{{ promo.name }}</option>
-            </select>
-        <div class="control-container">
-            <button href="#" @click=" createMember()">Create User</button>
-        </div>
-    </div>
+  <v-form ref="form" v-model="valid" lazy-validation>
+    <!-- First Name -->
+    <v-text-field
+      v-model="user.firstName"
+      :counter="10"
+      :rules="nameRules"
+      label="First Name"
+      required
+    />
+
+    <!-- Last Name -->
+    <v-text-field
+      v-model="user.lastName"
+      :counter="10"
+      :rules="nameRules"
+      label="Last Name"
+      required
+    />
+
+    <!-- Address -->
+    <v-text-field
+      v-model="user.address"
+      label="Address"
+      required
+    />
+
+    <!-- Address CP -->
+    <v-text-field
+      v-model="user.addressCP"
+      label="Postal Code"
+      required
+    />
+
+    <!-- Mail -->
+    <v-text-field
+      v-model="user.mail"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    />
+
+    <!-- Mobile -->
+    <v-text-field
+      v-model="user.mobileNumber"
+      label="Mobile"
+      :counter="12"
+      required
+    />
+
+    <!-- Pole Emploi Number -->
+    <v-text-field
+      v-model="user.poleEmploiNumber"
+      label="Pole emploi Id"
+      required
+    />
+
+    <!-- Promos -->
+    <v-select
+      v-model="user.namePromo"
+      :rules="getPromotionsNames(promos)"
+      :items="promotions"
+      label="Promo"
+      required
+    />
+
+    <v-checkbox
+      v-model="checkbox"
+      :rules="[v => !!v || 'You must agree to continue!']"
+      label="Do you agree?"
+      required
+    />
+
+    <v-btn :disabled="!valid" color="success" class="mr-4" @click="createMember(user)">Validate</v-btn>
+
+    <v-btn color="error" class="mr-4" @click="reset">Reset Form</v-btn>
+
+    <v-btn color="warning" @click="resetValidation">Reset Validation</v-btn>
+  </v-form>
 </template>
 
 <script>
-    import {
-        addMember
-    } from "../../services/api/member";
-    export default {
-        props: ["users", "promos"],
-        data() {
-            return {
-                title: 'New User',
-                user: {
-                    firstName: "",
-                    lastName: "",
-                    address: "",
-                    addressCP: "",
-                    mail: "",
-                    mobileNumber: "",
-                    poleEmploiNumber: "",
-                    namePromo: ""
-                }
-            }
-        },
-        methods: {
-            createMember() {
-                addMember(this.user.firstName, this.user.lastName, this.user.mail, this.user.address, this.user.addressCP, this.user.mobileNumber, this.user.poleEmploiNumber, this.user.namePromo)
-                    .then(() => {
-                        this.user.firstName = '';
-                        this.user.lastName = '';
-                        this.user.mail = '';
-                        this.user.address = '';
-                        this.user.addressCP = '';
-                        this.user.mobileNumber = '';
-                        this.user.poleEmploiNumber = '';
-                        this.user.namePromo = ''
-                    })
-                    .catch((error) => {
-                        this.error = error;
-                    });
-            }
-        }
+import { addMember } from "../../services/api/member";
+
+export default {
+  props: ["users", "promos"],
+  data: () => ({
+    valid: true,
+    user: {
+      firstName: "",
+      lastName: "",
+      address: "",
+      addressCP: "",
+      mail: "",
+      mobileNumber: "",
+      poleEmploiNumber: "",
+      namePromo: ""
+    },
+    promotions: [],
+    nameRules: [
+      v => !!v || "Name is required",
+      v => (v && v.length <= 10) || "Name must be less than 10 characters"
+    ],
+    emailRules: [
+      v => !!v || "E-mail is required",
+      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+    ],
+    select: null,
+    checkbox: false
+  }),
+  methods: {
+    validate() {
+      if (this.$refs.form.validate()) {
+        this.snackbar = true;
+      }
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
+    createMember(user) {
+      addMember(
+        user.firstName,
+        user.lastName,
+        user.mail,
+        user.address,
+        user.addressCP,
+        user.mobileNumber,
+        user.poleEmploiNumber,
+        user.namePromo
+      )
+        .then(() => {
+          this.validate();
+          user.firstName = "";
+          user.lastName = "";
+          user.mail = "";
+          user.address = "";
+          user.addressCP = "";
+          user.mobileNumber = "";
+          user.poleEmploiNumber = "";
+          user.namePromo = "";
+        })
+        .catch(error => {
+          this.error = error;
+        });
+    },
+    getPromotionsNames(promos)
+    {
+      for(const promo of promos)
+      {
+        if(this.promotions.includes(promo.name))
+          break;
+        else
+          this.promotions.push(promo.name);
+      }
     }
+  }
+};
 </script>
 
 <style scoped>
-    h1 {
-        background-color: #121212;
-        color: #fafafa;
-        width: 81%;
-    }
-    .form-control {
-        display: flex;
-        flex-direction: column;
-        width: 80%;
-        margin-left: 20%;
-    }
-    .control-container {
-        width: 100%;
-        display: flex;
-    }
-    .control-container>input {
-        width: 39.5%;
-    }
-    input {
-        width: 80%;
-        margin-bottom: 20px;
-    }
+form {
+  width: 50%;
+  margin-left: 20%;
+}
 </style>
