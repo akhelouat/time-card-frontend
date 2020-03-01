1096128ecacb14f62b7ff96fb821f9ccfaa9b3f7
KHELOUAT Abdelmalek <ab.khelouat@gmail.com>
2020-03-01 14:44:45 +0100

    improve the display of the update input and add a confirmation message after the modification

--- src/components/ManagerComponents/UserUpdater.vue
+++ src/components/ManagerComponents/UserUpdater.vue
@@ -5,8 +5,9 @@
           <h2>{{ user.firstName }}</h2><button v-if="penToHidden !== 'firstName'" @click="editDisplay('firstName')">Edit</button>
           <div v-if="edit === 'firstName'" class="updateValue">
               <input type="text" v-model="valueToUpdate"/>
-              <button @click="editDisplay()">Cancel</button>
-              <button @click="updateUser()">Edit</button>
+              <button class="cancelButton"  @click="editDisplay()">Cancel</button>
+              <button class="editButton"  @click="updateUser()">Edit</button>
+              <p class="updateResponse"> {{ updateResponse}} </p>
           </div>
       </div>
        <div class="information">
@@ -14,8 +15,10 @@
           <h2>{{ user.lastName }}</h2><button v-if="penToHidden !== 'lastName'" @click="editDisplay('lastName')">Edit</button>
           <div v-if="edit === 'lastName'" class="updateValue">
               <input type="text" v-model="valueToUpdate"/>
-              <button @click="editDisplay()" >Cancel</button>
-              <button @click="updateUser()">Edit</button>
+              <button class="cancelButton"  @click="editDisplay()" >Cancel</button>
+              <button class="editButton"  @click="updateUser()">Edit</button>
+                            <p class="updateResponse"> {{ updateResponse}} </p>
+
           </div>
       </div>
        <div class="information">
@@ -23,8 +26,10 @@
           <h2>{{ user.mail }}</h2><button v-if="penToHidden !== 'mail'" @click="editDisplay('mail')">Edit</button>
           <div v-if="edit === 'mail'" class="updateValue">
               <input type="text" v-model="valueToUpdate"/>
-              <button @click="editDisplay()">Cancel</button>
-              <button @click="updateUser()">Edit</button>
+              <button class="cancelButton"  @click="editDisplay()">Cancel</button>
+              <button class="editButton"  @click="updateUser()">Edit</button>
+                            <p class="updateResponse"> {{ updateResponse}} </p>
+
           </div>
       </div>
        <div class="information">
@@ -32,8 +37,10 @@
           <h2>{{ user.address }}</h2><button v-if="penToHidden !== 'address'" @click="editDisplay('address')">Edit</button>
           <div v-if="edit === 'address'" class="updateValue">
               <input type="text" v-model="valueToUpdate"/>
-              <button @click="editDisplay()">Cancel</button>
-              <button @click="updateUser()">Edit</button>
+              <button class="cancelButton"  @click="editDisplay()">Cancel</button>
+              <button class="editButton"  @click="updateUser()">Edit</button>
+                            <p class="updateResponse"> {{ updateResponse}} </p>
+
           </div>
       </div>
        <div class="information">
@@ -41,8 +48,10 @@
           <h2>{{ user.addressCP }}</h2><button v-if="penToHidden !== 'addressCP'" @click="editDisplay('addressCP')">Edit</button>
           <div v-if="edit === 'addressCP'" class="updateValue">
               <input type="text" v-model="valueToUpdate"/>
-              <button @click="editDisplay()">Cancel</button>
-              <button @click="updateUser()">Edit</button>
+              <button class="cancelButton" @click="editDisplay()">Cancel</button>
+              <button class="editButton" @click="updateUser()">Edit</button>
+                            <p class="updateResponse"> {{ updateResponse}} </p>
+
           </div>
       </div>
        <div class="information">
@@ -50,8 +59,10 @@
           <h2>{{ user.poleEmploiNumber }}</h2><button v-if="penToHidden !== 'poleEmploiNumber'" @click="editDisplay('poleEmploiNumber')">Edit</button>
           <div v-if="edit === 'poleEmploiNumber'" class="updateValue">
               <input type="text" v-model="valueToUpdate"/>
-              <button @click="editDisplay()">Cancel</button>
-              <button @click="updateUser()">Edit</button>
+              <button class="cancelButton"  @click="editDisplay()">Cancel</button>
+              <button class="editButton"  @click="updateUser()">Edit</button>
+                            <p class="updateResponse"> {{ updateResponse}} </p>
+
           </div>
       </div>
             <div class="information">
@@ -59,8 +70,10 @@
           <h2>{{ user.mobileNumber }}</h2><button v-if="penToHidden !== 'mobileNumber'" @click="editDisplay('mobileNumber')">Edit</button>
           <div v-if="edit ==='mobileNumber'" class="updateValue">
               <input type="text" v-model="valueToUpdate"/>
-              <button @click="editDisplay()">Cancel</button>
-              <button @click="updateUser()">Edit</button>
+              <button class="cancelButton"  @click="editDisplay()">Cancel</button>
+              <button class="editButton"  @click="updateUser()">Edit</button>
+                            <p class="updateResponse"> {{ updateResponse}} </p>
+
           </div>
       </div>
        <div class="information">
@@ -68,8 +81,10 @@
           <h2>{{ user.namePromo }}</h2><button v-if="penToHidden !== 'namePromo'" @click="editDisplay('namePromo')">Edit</button>
           <div v-if="edit === 'namePromo'" class="updateValue">
               <input type="text" v-model="valueToUpdate"/>
-              <button @click="editDisplay()">Cancel</button>
-              <button @click="updateUser()">Edit</button>
+              <button class="cancelButton"  @click="editDisplay()">Cancel</button>
+              <button class="editButton"  @click="updateUser()">Edit</button>
+                            <p class="updateResponse"> {{ updateResponse}} </p>
+
           </div>
       </div>
     </div>
@@ -87,7 +102,8 @@
                 penToHidden: null,
                 edit: null,
                 valueToUpdate: null,
-                paramsToUpdate: "firstName",
+                paramsToUpdate: "",
+                updateResponse: null,
                 users: {
                     firstName: "",
                     lastName: "",
@@ -98,7 +114,7 @@
         methods: { 
                        updateUser() {
                 updateMember(this.user._id, this.paramsToUpdate, this.valueToUpdate)
-                    .then(() => {
+                    .then((response) => { this.updateResponse = response
                     })
                     .catch((error) => {
                         this.error = error;
@@ -145,12 +161,27 @@
     font-size: 1.2em;
     color: green;
 }
-    input {
-        width: 80%;
-        margin-bottom: 20px;
-    }
-    h2,p{
-        margin: 1% 0 1% 5%;
-    }
+.cancelButton
+{
+    color: red
+}
+.editButton
+{
+    color: green;
+}
+input {
+    width: 60%;
+    margin-bottom: 20px;
+}
+h2,p{
+    margin: 1% 0 1% 5%;
+}
+.updateResponse{
+    background-color: #56bd56;
+    color: #003800;
+    padding: 1.5%;
+    display: inline-block;
+    border-radius: 10px;
+}
     
 </style>
