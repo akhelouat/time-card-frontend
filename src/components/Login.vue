70cb8a0225d5209f849b558fde4ffe21b811d381
stephane <stephane.vieira75@outlook.fr>
2020-03-02 21:07:42 +0100

    update: add vuetify login form in login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,52 +1,51 @@
 <template>
-    <div class="container">
-        <div class="form-control">
-            <div v-show="error">{{ error }}</div>
-            <input type="text" name="username" placeholder="username" v-model="user.username">
-            <input type="text" name="password" placeholder="password" v-model="user.password">
-            <button href="#" @click="getLoggedMember()">Log in</button>
-        </div>
-    </div>
+  <div id="app">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar color="#c61e42" flat>
+                <v-toolbar-title 
+                class="white--text">Login</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field label="Username" name="login" type="text" v-model="user.username" />
+                  <v-text-field id="password" label="Password" name="password" type="password" v-model="user.password" />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn class="white--text" color="#c61e42" @click="getLoggedMember()">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </div>
 </template>
 
 <script>
-    import { login } from '../services/api/member';
-
-    export default {
-        data: () => {
-            return {
-                user: {
-                    username: "",
-                    password: "",
-                },
-                error: ""
-            }
+  import {
+    login
+  } from '../services/api/member';
+  export default {
+    data: () => {
+      return {
+        user: {
+          username: "",
+          password: "",
         },
-        methods: {
-            isEmpty(usr) {
-                if((usr.name.length && usr.password.length) === 0 || (usr.name.length || usr.password.length) === 0)
-                    return this.error = "Error : empty values";
-                else
-                    return this.error = "Ok!";
-            },
-            getLoggedMember()
-            {
-                login(this.user.username, this.user.password)
-            }
-        }
+        error: ""
+      }
+    },
+    methods: {
+      getLoggedMember() {
+        login(this.user.username, this.user.password)
+      }
     }
+  }
 </script>
 
-<style scoped>
-    .container {
-        display: flex;
-        justify-content: center;
-    }
-    .form-control {
-        width: 20%;
-        padding: 15px;
-        display: flex;
-        flex-direction: column;
-        border: solid 1px #111;
-    }
-</style>
