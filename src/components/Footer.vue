f809c2d1857c622c9785d38dff3dc1c0ad571926
stephane <stephane.vieira75@outlook.fr>
2020-03-03 11:19:06 +0100

    create: footer and add him in the app

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-footer color="#2f2f2f" class="white--text" fixed="bottom">
+    
+    <v-row
+        cols="12"
+        align="center"
+        justify="center"
+    >
+        <v-img  max-width="10%" src="../assets/logo.png"></v-img>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
