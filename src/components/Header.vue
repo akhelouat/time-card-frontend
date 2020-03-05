ea0a9b9964c734d61d7876fe77d27a8d3307993a
stephane <stephane.vieira75@outlook.fr>
2020-03-05 11:19:51 +0100

    update: StatPromo display

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,12 @@
 <template>
-    <v-app-bar fixed dark color="#2f2f2f" elevate-on-scroll scroll-target="#scrolling-techniques-7">
+    <v-app-bar fixed dark color="#2f2f2f" elevate-on-scroll >
         <v-toolbar-title>
             <v-img width="10em" src="../assets/logo.png" />
         </v-toolbar-title>
         <v-spacer></v-spacer>
         <!-- Admin menu -->
         <div v-if="user.isAdmin === true">
-            <v-btn link color="#c61e42" class="white--text" :to="{name: 'adminhome'}">Home</v-btn>
+            <v-btn link color="#c61e42" class="white--text" :to="{name: 'statpromo'}">Home</v-btn>
             <v-btn link color="#c61e42" class="white--text" :to="{name: 'manager'}">Manager</v-btn>
             <v-btn link color="#c61e42" class="white--text" :to="{name: 'newpass'}">Change password</v-btn>
             <v-btn link color="#c61e42" class="white--text" :to="{name: 'login'}">Logout</v-btn>
